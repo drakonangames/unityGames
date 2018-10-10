f83927a86ac19013ba2038a59ffd4892a6abf75c
drakonan games <drakonangames@noreply.github.com>
2018-10-10 18:50:19 +0100

    Initial commit

---
+++ Roll-A-Zombie/Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour {
+
+	public void ClickMe()
+	{
+		SceneManager.LoadScene("MenuScreen");
+	}
+
+	public void Beginner()
+	{
+		SceneManager.LoadScene("BeginnerScene");
+	}
+
+	public void Easy()
+	{
+		SceneManager.LoadScene("EasyScene");
+	}
+
+	public void Medium()
+	{
+		SceneManager.LoadScene("MediumScene");
+	}
+
+	public void Hard()
+	{
+		SceneManager.LoadScene("HardScene");
+	}
+
+	public void Credits()
+	{
+		SceneManager.LoadScene("CreditsScene");
+	}
+
+	public void BackButton()
+	{
+		SceneManager.LoadScene("TitleScreen");
+	}
+
+	public void QuitButton()
+	{
+		Application.Quit();
+	}
+}
---
+++ Roll-A-Zombie/Assets/Scripts/playMusic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class playMusic : MonoBehaviour {
+
+	public static playMusic Instance;
+	void Awake()
+	{
+
+		if (Instance)
+			DestroyImmediate (gameObject);
+		else {
+			DontDestroyOnLoad (gameObject);
+			Instance = this;
+
+		}
+	}
+	
+}
