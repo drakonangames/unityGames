ad7f5c1869aa44756f808c75aa543d1e041f29fe
drakonan games <44027614+drakonangames@users.noreply.github.com>
2018-10-18 22:49:25 +0100

    BeatBoxV1

---
+++ BeatBox/Assets/Scripts/Close.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Close : MonoBehaviour {
+
+	// Use this for initialization
+	public void QuitApp()
+    {
+        Application.Quit();
+        Debug.Log("Quited");
+    }
+}
